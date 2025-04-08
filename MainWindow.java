66348ad6dd76d8c9f745d7369a90974c4b0c4d2f
itachi2008aaa <112188377+itachi2008aaa@users.noreply.github.com>
2025-04-08 17:45:52 +0200

    Alex

---
+++ MainWindow.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    private String currentUser; // the username of the logged-in worker
+
+    public MainWindow(String currentUser) {
+        this.currentUser = currentUser;
+        setTitle("Grocery Store Management - Logged in as: " + currentUser);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+
+        // Create tabs
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        tabbedPane.addTab("Einkauf erstellen", new CreateReceiptPanel(currentUser));
+
+
+        add(tabbedPane, BorderLayout.CENTER);
+    }
+}
